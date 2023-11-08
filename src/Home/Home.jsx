4e10ae5adf3c320609e2085f9c346c5deedba04f
SantiagoMisas <santiagomisaspulgarin@gmail.com>
2023-11-07 20:52:50 -0500

    Se añanden componentes: tarjetas, carrusel usando Swiper y se añade la imagen de fondo como prueba

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,30 +1,23 @@
 import "./Home.css";
 import { Formulario } from "../Formulario/Formulario";
+import { Carusel } from "../Carusel/Carusel";
+import { Tarjetas } from "../Tarjetas/Tarjetas";
 export function Home() {
   return (
     <>
       <header>
-        <nav class="navbar navbar-expand-lg menu ">
-          <div class="container-fluid">
-          
-
-            <a class="navbar-brand" href="#">
-                <div class="text-center text-">
-                    <img src="../../src/assets/img/abarrotes.png" class="img-fluid d-flex" alt="Ícono de Búsqueda" width="120px"/>
-                    <span className="text-center" >  Abarrotes La Esperanza</span>
-                </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid menu">
+            <a className="navbar-brand" href="#">
+              <img
+                src="../../src/assets/img/abarrotes.png"
+                alt=""
+                width="100"
+                height="100"
+              />
             </a>
-
-            <form class="d-flex" role="search">
-                <div class="form-outline">
-                    <input type="text" class="form-control ps-5" placeholder="Buscar mercado" />
-                    <i class="bi bi-search ms-3"></i>
-                </div>                  
-                <button class="btn btn-outline-dark" type="submit">Search</button>
-            </form>
-
             <button
-              class="navbar-toggler"
+              className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#navbarSupportedContent"
@@ -32,170 +25,158 @@ export function Home() {
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
-              <span class="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item border-end">
-                  <a class="nav-link active" aria-current="page" href="#">
-                  ¡Contactános!
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <a className="nav-link active" aria-current="page" href="#">
+                    Inicio
                   </a>
                 </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">
-                  Reseña
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Servicio al cliente
                   </a>
                 </li>
-                <li class="nav-item dropdown">
-                  <a
-                    class="nav-link dropdown-toggle"
-                    href="#"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Dropdown
-                  </a>
-                  <ul class="dropdown-menu">
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <hr class="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a class="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled" aria-disabled="true">
-                    Disabled
+                <li className="nav-item">
+                  <a className="nav-link disabled" href="#">
+                    Reseña
                   </a>
                 </li>
               </ul>
-             
-                        
-              
+              <form className="d-flex">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button className="btn btn-outline-success" type="submit">
+                  Search
+                </button>
+              </form>
             </div>
           </div>
         </nav>
       </header>
 
-
-
       <main>
         <section>
-          <div className="container-fluid p-5">
-            <div className="row">
-              <div className="col-12 col-md-3  text-center">
-                <div className="row my-3 border p-3">
-                  <div class="dropdown">
-                    <h5
-                      class=" mx-auto d-block dropdown-toggle"
-                      type="button"
-                      data-bs-toggle="dropdown"
-                    >
-                      Opcion 1
-                    </h5>
+          <div className="banner text-white fw-bold">
+            <div className="container-fluid p-5">
+              <div className="row">
+                <div className="col-12 col-md-3  text-center">
+                  <div className="row my-3 border p-3">
+                    <div className="dropdown">
+                      <h5
+                        className=" mx-auto d-block dropdown-toggle"
+                        type="button"
+                        data-bs-toggle="dropdown"
+                      >
+                        Opcion 1
+                      </h5>
 
-                    <ul class="dropdown-menu">
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          Action
-                        </a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          Another action
-                        </a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          Something else here
-                        </a>
-                      </li>
-                    </ul>
+                      <ul className="dropdown-menu">
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Action
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Another action
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Something else here
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="row my-3 border p-3">
+                    <div className="dropdown">
+                      <h5
+                        className=" mx-auto d-block dropdown-toggle"
+                        type="button"
+                        data-bs-toggle="dropdown"
+                      >
+                        Opcion 1
+                      </h5>
+                      <ul className="dropdown-menu">
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Action
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Another action
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Something else here
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="row my-3 border p-3">
+                    <div className="dropdown">
+                      <button
+                        className="btn btn-secondary dropdown-toggle"
+                        type="button"
+                        data-bs-toggle="dropdown"
+                        aria-expanded="false"
+                      >
+                        Dropdown button
+                      </button>
+                      <ul className="dropdown-menu">
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Action
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Another action
+                          </a>
+                        </li>
+                        <li>
+                          <a className="dropdown-item" href="#">
+                            Something else here
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
-                <div className="row my-3 border p-3">
-                  <div class="dropdown">
-                    <h5
-                      class=" mx-auto d-block dropdown-toggle"
-                      type="button"
-                      data-bs-toggle="dropdown"
-                    >
-                      Opcion 1
-                    </h5>
-                    <ul class="dropdown-menu">
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          Action
-                        </a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          Another action
-                        </a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          Something else here
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
+                <div className="col-12 col-md-9 bg-dark">
+                  <img
+                    src="../../src/assets/img/bannerprueba.PNG"
+                    alt=""
+                    className="img-fluid w-100"
+                  />
                 </div>
-                <div className="row my-3 border p-3">
-                  <div class="dropdown">
-                    <button
-                      class="btn btn-secondary dropdown-toggle"
-                      type="button"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      Dropdown button
-                    </button>
-                    <ul class="dropdown-menu">
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          Action
-                        </a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          Another action
-                        </a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          Something else here
-                        </a>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-              <div className="col-12 col-md-9 bg-dark">
-                <img
-                  src="../../src/assets/img/bannerprueba.PNG"
-                  alt=""
-                  className="img-fluid w-100"
-                />
               </div>
             </div>
           </div>
         </section>
       </main>
+
+      <section>
+        <Carusel></Carusel>
+      </section>
+
+      <section>
+        <Tarjetas></Tarjetas>
+      </section>
     </>
   );
 }
